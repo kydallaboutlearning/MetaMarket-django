a23cd9bc1f4e67e8022f186cb99ef9ea3f07200f
kydallaboutlearning <kydallaboutlearning@gmail.com>
2024-11-30 17:37:39 +0100

    Achievement: Added gettext_lazy for translation of the urls

--- orders/urls.py
+++ orders/urls.py
@@ -2,10 +2,15 @@
 from django.urls import path
 from .views import CreateOrderView,admin_order_detail, admin_order_pdf
 
+#importing gettext_lazy for url translation
+from django.utils.translation import gettext_lazy as _
+
+
+
 app_name = 'orders'
 
 urlpatterns = [
-    path('order_create/',CreateOrderView, name = 'create_order'),
-    path('admin/orders/<int:order_id>',admin_order_detail, name = 'admin_order_detail'),
-    path('admin/orders/<int:order_id>/pdf/',admin_order_pdf,name = 'admin_order_pdf')
+    path(_('order_create/'),CreateOrderView, name = 'create_order'),
+    path(_('admin/orders/<int:order_id>'),admin_order_detail, name = 'admin_order_detail'),
+    path(_('admin/orders/<int:order_id>/pdf/'),admin_order_pdf,name = 'admin_order_pdf')
 ]
