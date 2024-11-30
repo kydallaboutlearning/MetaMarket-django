7eee238fdeeec1949de7e7d1d21601a9c760906d
kydallaboutlearning <kydallaboutlearning@gmail.com>
2024-11-30 17:36:59 +0100

    Achievement: Added gettext_lazy for translation of the urls

--- coupons/urls.py
+++ coupons/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import couponapply
+from django.utils.translation import gettext_lazy as _
+
 
 #adding the urls
 app_name = 'coupons'
 
 urlpatterns = [
-    path('apply_coupon/',couponapply, name = 'apply')
+    path(_('apply_coupon/'),couponapply, name = 'apply')
 ]
