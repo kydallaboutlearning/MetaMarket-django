518dc406aaeb9bffd38a2c944a83ffefb4fc3fbb
kydallaboutlearning <kydallaboutlearning@gmail.com>
2024-11-30 17:35:16 +0100

    Achievement: Added gettext_lazy for translation

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,9 @@ from django.urls import path,include
 from django.urls import reverse
 from django.contrib.auth import views as auth_views
 import django
+#importing gettextlazy for translation
+from django.utils.translation import gettext_lazy as _
+
 
 #importing views
 from .views import RegisterView,EditView,LogoutView
@@ -12,7 +15,7 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('',include('django.contrib.auth.urls')),
-    path('logout_form/user/', LogoutView,name = 'logout_form'),
-    path('register/user/',RegisterView,name = 'register'),
-    path('edit_profile/', EditView, name = 'edit_profile')
+    path(_('logout_form/user/'), LogoutView,name = 'logout_form'),
+    path(_('register/user/'),RegisterView,name = 'register'),
+    path(_('edit_profile/'), EditView, name = 'edit_profile')
 ]
