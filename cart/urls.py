72c67e5e1b7bf04aa0552bb6af624fb88d2dc0c4
kydallaboutlearning <kydallaboutlearning@gmail.com>
2024-11-30 17:36:15 +0100

    Achievement: Added gettext_lazy for translation of the urls

--- cart/urls.py
+++ cart/urls.py
@@ -4,11 +4,14 @@ from .views import (cart_detail,
                    cart_remove
                    )
 
+#importing gettext_lazy for translation
+from django.utils.translation import gettext_lazy as _
+
 app_name = 'cart'
 
 #adding the paths
 urlpatterns = [
     path('', cart_detail, name = 'cart_detail' ),
-    path('add_to_cart/<int:product_id>/', cart_add, name ='add_to_cart'),
-    path('remove/<int:product_id>/', cart_remove, name = 'remove_cart')
+    path(_('add_to_cart/<int:product_id>/'), cart_add, name ='add_to_cart'),
+    path(_('remove/<int:product_id>/'), cart_remove, name = 'remove_cart')
 ]
