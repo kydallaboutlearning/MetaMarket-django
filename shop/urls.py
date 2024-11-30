322b25f639314efc2b3d1f3e864e6d8ecf168ddb
kydallaboutlearning <kydallaboutlearning@gmail.com>
2024-11-30 17:39:45 +0100

    Achievement: Added gettext_lazy for translation of the urls

--- shop/urls.py
+++ shop/urls.py
@@ -2,10 +2,15 @@
 from django.urls import path
 from . import views
 
+
+#importing gettextlazy to enable url translation
+from django.utils.translation import gettext_lazy as _
+
+
 app_name = 'shop'
 
 urlpatterns = [
     path('',views.product_list,name = 'product_list'),
-    path('<slug:category_slug>',views.product_list,name = 'product_list_by_category'),
-    path('<int:id>/<slug:slug>/',views.product_details,name = 'product_detail'),
+    path(_('<slug:category_slug>'),views.product_list,name = 'product_list_by_category'),
+    path(_('<int:id>/<slug:slug>/'),views.product_details,name = 'product_detail'),
 ]
