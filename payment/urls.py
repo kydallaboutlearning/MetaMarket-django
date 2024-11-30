74b4d9798fad3e679f77b7819a6722c56fb86d55
kydallaboutlearning <kydallaboutlearning@gmail.com>
2024-11-30 17:38:47 +0100

    Achievement: Added gettext_lazy for translation of the urls

--- payment/urls.py
+++ payment/urls.py
@@ -2,14 +2,19 @@ from django.urls import path
 from .views import *
 from .webhooks import  *
 
+#importing gettextlazy for url translation
+from django.utils.translation import gettext_lazy as _
+
+
+
 #defining the url patterns
 
 #adding app name
 app_name = 'payment'
 
+
 urlpatterns = [
-    path('process/',payment_process, name = 'process'),
-    path('complete/',payment_completed, name = 'completed'),
-    path('cancel/',payment_cancel, name = 'cancel'),
-    path('webhook/',stripe_webhook, name='stripe-webhook'),
+    path(_('process/'),payment_process, name = 'process'),
+    path(_('complete/'),payment_completed, name = 'completed'),
+    path(_('cancel/'),payment_cancel, name = 'cancel'),
 ]
