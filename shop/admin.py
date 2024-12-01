93907ce7f2a02003d2fcce309018931be05e2137
kydallaboutlearning <kydallaboutlearning@gmail.com>
2024-12-01 19:01:06 +0100

    Achievement: Added Django parler to add the category model to the translatable admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 from .models import Category,Product
+#importing parler to add translatable admin
+from parler.admin import TranslatableAdmin
 
 # Register your models here.
 @admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
+class CategoryAdmin(TranslatableAdmin):
     list_display = ('name', 'slug')
     
     #prepolulated fields help automatically gives values which is slug in this case
-    prepopulated_fields ={'slug': ('name',)}
+    def get_prepopulated_fields(self, request, obj=None):
+        return {'slug': ('name',)}
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
