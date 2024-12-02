5da36cd9875d9a7b235e493cdcf6c6575365af44
kydallaboutlearning <kydallaboutlearning@gmail.com>
2024-12-02 22:05:14 +0100

    Achievement: Able to make the dev mode work but having issue with css

--- Metamarket/manage.py
+++ Metamarket/manage.py
@@ -3,10 +3,12 @@
 import os
 import sys
 
+from decouple import config
+
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Metamarket.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', default='Metamarket.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
