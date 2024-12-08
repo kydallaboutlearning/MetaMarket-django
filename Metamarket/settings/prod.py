b27ef4ee824c9e057e798e6b7838d109eda9dfb5
kydallaboutlearning <kydallaboutlearning@gmail.com>
2024-12-08 20:19:49 +0100

    Changes: Added the postgres db

--- Metamarket/settings/prod.py
+++ Metamarket/settings/prod.py
@@ -1,5 +1,5 @@
 from .base import *
-
+from decouple import config
 
 DEBUG = False
 ADMINS = [
@@ -8,9 +8,15 @@ ADMINS = [
 ALLOWED_HOSTS = ['*']
 
 
+
+#addin the postgres db
 DATABASES = {
  'default': {
- 'ENGINE': 'django.db.backends.sqlite3',
- 'NAME': BASE_DIR / 'db.sqlite3',
+ 'ENGINE': 'django.db.backends.postgresql',
+ 'NAME': config('POSTGRES_DB'),
+ 'USER': config('POSTGRES_USER'),
+ 'PASSWORD': config('POSTGRES_PASSWORD'),
+ 'HOST': 'db',
+ 'PORT': 5432,
  }
 }
