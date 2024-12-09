08bdf3758068cc37d78a2fce27ab308bf33228eb
kydallaboutlearning <kydallaboutlearning@gmail.com>
2024-12-09 10:56:35 +0100

    Achievement: Added the postgres_db

--- Metamarket/settings/prod.py
+++ Metamarket/settings/prod.py
@@ -1,22 +1,31 @@
 from .base import *
 from decouple import config
 
+
+
 DEBUG = False
+
+
+
+
+
+
+
+
 ADMINS = [
  ('KYD', 'kydallaboutlearning@gmail.com'),
 ]
 ALLOWED_HOSTS = ['*']
 
 
-
-#addin the postgres db
+# Database Configuration
 DATABASES = {
- 'default': {
- 'ENGINE': 'django.db.backends.postgresql',
- 'NAME': config('POSTGRES_DB'),
- 'USER': config('POSTGRES_USER'),
- 'PASSWORD': config('POSTGRES_PASSWORD'),
- 'HOST': 'db',
- 'PORT': 5432,
- }
+    "default": {
+    'ENGINE': 'django.db.backends.postgresql',
+ 	'NAME': config('POSTGRES_DB'),
+ 	'USER': config('POSTGRES_USER'),
+ 	'PASSWORD': config('POSTGRES_PASSWORD'),
+ 	'HOST': 'db',
+ 	'PORT': 5432,
+    }
 }
