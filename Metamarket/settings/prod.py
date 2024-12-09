c03b36b3f21c8426579fc9b9a61196cee3492458
kydallaboutlearning <kydallaboutlearning@gmail.com>
2024-12-09 15:08:38 +0100

    Achievement:  Added redis to the production settings file

--- Metamarket/settings/prod.py
+++ Metamarket/settings/prod.py
@@ -1,31 +1,40 @@
 from .base import *
 from decouple import config
 
-
-
 DEBUG = False
 
-
-
-
-
-
-
-
 ADMINS = [
- ('KYD', 'kydallaboutlearning@gmail.com'),
+    ('KYD', 'kydallaboutlearning@gmail.com'),
 ]
 ALLOWED_HOSTS = ['*']
-
 
 # Database Configuration
 DATABASES = {
     "default": {
-    'ENGINE': 'django.db.backends.postgresql',
- 	'NAME': config('POSTGRES_DB'),
- 	'USER': config('POSTGRES_USER'),
- 	'PASSWORD': config('POSTGRES_PASSWORD'),
- 	'HOST': 'db',
- 	'PORT': 5432,
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('POSTGRES_DB'),
+        'USER': config('POSTGRES_USER'),
+        'PASSWORD': config('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
     }
+}
+
+# Setting up cache
+REDIS_URL = 'redis://cache:6379'
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': REDIS_URL,
+    }
+}
+
+# Setting up channel layers
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [REDIS_URL],
+        },
+    },
 }
