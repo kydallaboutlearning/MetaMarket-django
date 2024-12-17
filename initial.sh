bcb29b97282a7f461e07e9cae78bad163b1597d7
kydallaboutlearning <kydallaboutlearning@gmail.com>
2024-12-17 16:22:29 +0100

    Changes: Added this to help initialize

---
+++ initial.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env bash
+
+# Let the DB start
+python init_db.py
+
+until pg_isready -h db -U postgres; do
+  echo "Waiting for the database to be ready..."
+  sleep 1
+done
+
+# Run migrations
+python manage.py migrate --no-input
+
+# run tests
+pytest --disable-warnings -vv -x
+
+
+# starts application
+
+python manage.py runserver 0.0.0.0:8000
