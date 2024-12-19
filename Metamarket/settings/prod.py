a1d78cdf76af725a0d3a790d7c892e14acc6482e
kydallaboutlearning <kydallaboutlearning@gmail.com>
2024-12-19 06:38:37 +0100

    Achievement: Added postgres_sql

--- Metamarket/settings/prod.py
+++ Metamarket/settings/prod.py
@@ -11,8 +11,12 @@ ALLOWED_HOSTS = ['*']
 # Database Configuration
 DATABASES = {
     "default": {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('POSTGRES_DB'),
+        'USER': config('POSTGRES_USER'),
+        'PASSWORD': config('POSTGRES_PASSWORD'),
+        'HOST': config('POSTGRES_HOST'),
+        'PORT': config('POSTGRES_PORT')
     }
 }
 
